2fdace7b283b1023b01143a46dbd590d92c24b32
schlucht <web@jagolo.ch>
2022-01-25 05:51:59 +0100

    View Componente

---
+++ views/otsLogo/otsLogoStyle.ts
@@ -0,0 +1,21 @@
+const otsLogoStyle = `
+<style>
+.logo {
+    font-size: 6rem;
+    font-weight: 300;
+    position: absolute;
+    left: 0;
+    top: 0;
+}
+.logo .logo-text {
+    border: solid 1px red;
+    width: 100%;
+    margin: 0;
+}
+.logo .logo-text span {
+    color: var(--col-green);
+    font-size: 10rem;
+}
+</style>
+`
+export default otsLogoStyle
---
+++ views/otsLogo/otsLogoView.ts
@@ -0,0 +1,8 @@
+import otsLogoStyle from "./otsLogoStyle"
+const  otsLogoView = `
+${otsLogoStyle}
+<div class="logo">
+    <span class="logo-text">Delta-<span>V</span></span>
+</div>`
+
+export default otsLogoView 
