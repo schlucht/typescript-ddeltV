5d0b17ee4b88a6f60916603adfb02c232e5e1f82
schlucht <lothi@schmidschlucht.ch>
2022-03-01 12:36:27 +0100

    2 files modified, 7 deleted

--- views/otsLogo/otsLogoView.ts
+++
@@ -1,8 +0,0 @@
-import otsLogoStyle from "./otsLogoStyle"
-const  otsLogoView = `
-${otsLogoStyle}
-<div class="logo">
-    <span class="logo-text">Delta-<span>V</span></span>
-</div>`
-
-export default otsLogoView 
